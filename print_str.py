d5b11542d4928113f7bc0c424e529dbc159d530c
Mr Wang <m13wang@gmail.com>
2013-08-09 18:42:13 -0600

    Initial commit
    3 files that are unassociated.

---
+++ print_str.py
@@ -0,0 +1,11 @@
+def run():
+    """
+    print each letter in the string using the for loop syntax and then return the last character sequence
+    """
+    str = "LearnStreet!"
+    for i in str:
+        print i
+    return i
+
+#This is just for you to see what happens when the function is called
+run()
