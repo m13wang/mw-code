acb8d5238fa8d032cdc2b2273632bccf7de6736f
Mr Wang <m13wang@gmail.com>
2013-08-09 19:04:20 -0600

    Rev 2.2  only changed one file

--- print_str linked file.py
+++ print_str linked file.py
@@ -3,3 +3,5 @@ def run():
     """
     print each letter in the string using the for loop syntax and then return the last character sequence
     """
+
+#change for rev 2.1  but not changing the main file
